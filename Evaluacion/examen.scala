311ea81bd0fb8baffea38ea3c5e4555d2b926c29
AlexisBs <54299559+AlexisBs@users.noreply.github.com>
2020-02-26 18:12:04 -0800

    Update Test#1

--- Evaluacion/examen.scala
+++ Evaluacion/examen.scala
@@ -5,7 +5,7 @@
 val arre = ((11,2,4),(4,5,6),(10,8,-12))
 
 
-def difabs(arre2:((Int, Int, Int), (Int, Int, Int), (Int, Int, Int))): Int = {
+def difabs(arre:((Int, Int, Int), (Int, Int, Int), (Int, Int, Int))): Int = {
     val diagonl_1 =(arre._1._1)+(arre._2._2)+(arre._3._3)
     val diagonl_2 =(arre._1._3)+(arre._2._2)+(arre._3._1)
     var diferencia = diagonl_1-diagonl_2
